554e5e5e436c8a98e055c047991af5863f0906ae
Donald Kiser <kiserd@oregonstate.edu>
2022-01-04 18:31:14 -0800

    added comments, removed debug lines

--- simple-text-editor/editor.py
+++ simple-text-editor/editor.py
@@ -2,27 +2,31 @@
 import sys
 
 def editor():
+    # use file input while working in IDE
     with open('input2', 'r') as file:
         num_ops = int(file.readline().strip())
         s = ''
         stack = []
         for i in range(num_ops):
-            # print('s: ', s)
-            # print('s: ', s)
+            # get operation number and idx/string
             op = file.readline().split()
+            # if only one input item, undo last action
             if len(op) == 1:
                 s = stack.pop()
             else:
                 op, idx = op
+                # append new string to end of s
                 if int(op) == 1:
                     stack.append(s)
                     s += idx
+                # remove last idx characters from s
                 elif int(op) == 2:
                     stack.append(s)
                     new_s = ''
                     for i in range(len(s) - (int(idx))):
                         new_s += s[i]
                     s = new_s
+                # print desired character
                 else:
                     print(s[int(idx) - 1])
 
